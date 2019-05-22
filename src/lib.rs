2c1d87c26c8de8aa272a2589ad57c22bfb9912a4
suhanyujie <suhanyujie@qq.com>
2019-05-22 15:34:44 +0800

    php2rust var_dump

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+use std::fmt::{Debug};
+
+fn var_dump<T:Debug>(arg: &T) {
+    println!("{:?}", arg);
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_var_dump() {
+        let x:i32 = 12;
+        var_dump(&x);
+        assert!(true);
     }
 }
